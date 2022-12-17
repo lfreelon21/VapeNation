b173a301560a8db9477838e7078c0e5518eeea82
lfreelon21 <leibra0120@gmail.com>
2022-12-17 03:07:23 -0800

    added use animation to go green vape

--- src/main/java/net/lf3/vapenation/items/ModItems.java
+++ src/main/java/net/lf3/vapenation/items/ModItems.java
@@ -4,6 +4,8 @@ import net.lf3.vapenation.VapeNation;
 import net.lf3.vapenation.items.custom.GoGreenVapeItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.UseAnim;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.Material;
@@ -17,4 +19,5 @@ public class ModItems {
 
     public static final RegistryObject<Item> GO_GREEN_VAPE = ITEMS.register("go_green_vape",
             () -> new GoGreenVapeItem(new Item.Properties().tab(CreativeModeTab.TAB_MISC).stacksTo(1)));
+
 }
--- src/main/java/net/lf3/vapenation/items/custom/GoGreenVapeItem.java
+++ src/main/java/net/lf3/vapenation/items/custom/GoGreenVapeItem.java
@@ -8,21 +8,34 @@ import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.ItemUtils;
+import net.minecraft.world.item.UseAnim;
 import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.NotNull;
 
 public class GoGreenVapeItem extends Item {
+    private static final int DRINK_DURATION = 32;
     public GoGreenVapeItem(Properties p){
         super(p);
     }
 
     @Override
     public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand) {
+        //play go green sound
         level.playSound(null, new BlockPos(player.blockPosition()), ModSounds.GO_GREEN_SOUND.get(), SoundSource.PLAYERS, 1, 1);
         //use vape
-        //play go green sound
+        ItemStack vapeStack = player.getMainHandItem();
         //output vape clouds
         //set cooldown
 
-        return super.use(level, player, hand);
+        return ItemUtils.startUsingInstantly(level, player, hand);
+    }
+    @Override
+    public @NotNull UseAnim getUseAnimation(ItemStack itemstack){
+        return UseAnim.TOOT_HORN;
+    }
+    @Override
+    public int getUseDuration(ItemStack itemstack){
+        return 32;
     }
 }
